98ed6f8c51f9e188a1238dd478c64a257e8e2bf6
DeepCFoxXx <learncode23@gmail.com>
2018-04-12 16:00:29 +0100

    record price passing

--- specs/record_spec.js
+++ specs/record_spec.js
@@ -3,18 +3,22 @@ var Record = require('../record.js');
 
 describe('Record', function() {
 
- var record1;
+  var record1;
 
   beforeEach(function() {
     record1 = new Record('Queens of the stone age', 'Songs for the deaf', 9.99);
   });
 
-  it('should have an artist', function() {
+  it('should have a artist', function() {
     assert.equal(record1.artist, 'Queens of the stone age');
   });
 
-  it('should have an title', function() {
+  it('should have a title', function() {
     assert.equal(record1.title, 'Songs for the deaf');
+  });
+
+  it('should have a price', function() {
+    assert.equal(record1.price, 9.99);
   });
 
 });
