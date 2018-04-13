4680f5cb61fcff2c6e8654eeb6efb0c0ef3c831c
DeepCFoxXx <learncode23@gmail.com>
2018-04-13 10:45:24 +0100

    store name passing

--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -1,17 +1,21 @@
-var assert = require("assert");
-var RecordStore = require("../record_store.js");
-var Record = require("../record.js");
+var assert = require('assert');
+var RecordStore = require('../record_store.js');
+var Record = require('../record.js');
 
-describe("Record Store tests", function() {
+describe('Record Store tests', function() {
 
   var recordStore;
   var record1;
   var record2;
 
-  beforeEach("Setup", function(){
-    recordStore = new RecordStore("Deaf Factory", "Edinburgh");
+  beforeEach('Setup', function(){
+    recordStore = new RecordStore('Deaf Factory', 'Edinburgh');
     record1 = new Record('Queens of the stone age', 'Songs for the deaf', 9.99);
-    record2 = new Record("Marina and the diamonds", "Family jewels", 9.99);
+    record2 = new Record('Marina and the diamonds', 'Family jewels', 9.99);
+  });
+
+  it('should have a name', function() {
+    assert.equal(recordStore.name, 'Deaf Factory');
   });
 
 });
